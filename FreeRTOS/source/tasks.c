32153e9a70974b5283c7cccab110dfc5003fc057
Ricardo <tsao.ricardo@iac.com.tw>
2011-09-21 14:55:38 +0800

    Modify task.c
    // Ricardo modify
    #if 0
    pxTopOfStack = ( portSTACK_TYPE * ) ( ( ( portPOINTER_SIZE_TYPE ) pxTopOfStack ) & ( ( portPOINTER_SIZE_TYPE ) ~portBYTE_ALIGNMENT_MASK  ) );
    #else
    pxTopOfStack = ( portSTACK_TYPE * ) ( ( ( portSTACK_TYPE ) pxTopOfStack ) & ( ( portSTACK_TYPE ) ~portBYTE_ALIGNMENT_MASK  ) );
    #endif
    
    Signed-off-by: Ricardo <tsao.ricardo@iac.com.tw>

--- FreeRTOS/source/tasks.c
+++ FreeRTOS/source/tasks.c
@@ -477,7 +477,13 @@ tskTCB * pxNewTCB;
 		#if( portSTACK_GROWTH < 0 )
 		{
 			pxTopOfStack = pxNewTCB->pxStack + ( usStackDepth - ( unsigned short ) 1 );
+
+            // Ricardo modify
+            #if 0
 			pxTopOfStack = ( portSTACK_TYPE * ) ( ( ( portPOINTER_SIZE_TYPE ) pxTopOfStack ) & ( ( portPOINTER_SIZE_TYPE ) ~portBYTE_ALIGNMENT_MASK  ) );
+            #else
+            pxTopOfStack = ( portSTACK_TYPE * ) ( ( ( portSTACK_TYPE ) pxTopOfStack ) & ( ( portSTACK_TYPE ) ~portBYTE_ALIGNMENT_MASK  ) );
+            #endif
 
 			/* Check the alignment of the calculated top of stack is correct. */
 			configASSERT( ( ( ( unsigned long ) pxTopOfStack & ( unsigned long ) portBYTE_ALIGNMENT_MASK ) == 0UL ) );
